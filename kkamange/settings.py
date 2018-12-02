28a5eac6377304b0571d5cd14bea0471231b6995
productserver <vovoboss@gmail.com>
2018-12-02 16:07:37 +0900

    change db

--- kkamange/settings.py
+++ kkamange/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'kkamange.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DATABASE_NAME'),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'HOST':'localhost',
     }
 }
 
