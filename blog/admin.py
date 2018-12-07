ec3ef8c27705a01b79e500d587f3a52fdbf952e5
vovo <vovoboss@gmail.com>
2018-12-07 14:30:42 +0900

    add link

--- blog/admin.py
+++ blog/admin.py
@@ -5,4 +5,5 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'content', 'image_tag', 'thumbnail']
+    list_display_links = ['image_tag']
     readonly_fields = ['image_tag']
