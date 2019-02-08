61021c935d7bc027c0adfe39e4128fb0b35846f8
dizzyplay <vovoboss@gmail.com>
2019-02-08 20:16:24 +0900

    Add auth setting

--- kkamange/adapters.py
+++ kkamange/adapters.py
@@ -1,5 +1,6 @@
 from allauth.socialaccount.providers.naver.views import NaverOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
 
+
 class NaverLogin(SocialLoginView):
     adapter_class = NaverOAuth2Adapter
